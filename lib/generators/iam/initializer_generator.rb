a529838af5f4cf76697e53036300eddb97af78f5
Yury Kaliada <fut.wrk@gmail.com>
2012-12-02 12:18:10 +0300

    Configuration inializer generator added
    Signed-off-by: Yury Kaliada <fut.wrk@gmail.com>

---
+++ lib/generators/iam/initializer_generator.rb
@@ -0,0 +1,15 @@
+module Iam
+  module Generators
+    class InitializerGenerator < ::Rails::Generators::Base
+
+      source_root File.expand_path("../../templates", __FILE__)
+
+      desc 'Creates a sample Iam initializer.'
+
+      def copy_initializer
+        copy_file 'iam.rb', 'config/initializers/iam.rb'
+      end
+
+    end
+  end
+end
