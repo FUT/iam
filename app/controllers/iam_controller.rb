c0556efaa1ea823d266c40514b98c3c4c2370554
Yury Koleda <yury.koleda@altoros.com>
2012-12-04 08:52:19 +0300

    Security fix
    Signed-off-by: Yury Koleda <yury.koleda@altoros.com>

--- app/controllers/iam_controller.rb
+++ app/controllers/iam_controller.rb
@@ -1,5 +1,7 @@
 class IamController < ApplicationController
   def log_in_as
+    return if Rail.env == 'production'
+
     account = Iam::Configuration.account_class.constantize.find(params[:id])
     sign_in Iam::Configuration.account_class.downcase, account
 
