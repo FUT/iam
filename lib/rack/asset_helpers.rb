2cecdf079e18fa26412288bd51be7b467dbaf316
Yury Kaliada <fut.wrk@gmail.com>
2012-12-10 13:11:43 +0300

    JS & CSS duplication fixed
    Signed-off-by: Yury Kaliada <fut.wrk@gmail.com>

--- lib/rack/asset_helpers.rb
+++ lib/rack/asset_helpers.rb
@@ -6,22 +6,20 @@ module Rack
 
     private
     def css_code
-      '<style type="text/css">' << typed_asset('stylesheets', 'iam.css') << '</style>'
+      "<style type='text/css'>#{typed_asset('stylesheets', 'iam.css')}</style>"
     end
 
     def js_code
-      '<script type="text/javascript">' << typed_asset('javascripts', 'iam.js', 'jquery.cookie.js') << '</script>'
+      "<script type='text/javascript'>#{typed_asset('javascripts', 'iam.js', 'jquery.cookie.js')}</script>"
     end
 
     def typed_asset(type, *files)
-      files.map { |f| asset type, f }.reduce :<<
+      files.map { |f| asset type, f }.reduce :+
     end
 
     def asset(type, file)
       @assets ||= {}
-      output = ::File.open(::File.join(::File.dirname(__FILE__), '..', '..', 'vendor', 'assets', type, file), 'r:UTF-8') do |f|
-        f.read
-      end
+      output = ::File.read ::File.join(::File.dirname(__FILE__), '..', '..', 'vendor', 'assets', type, file)
       @assets[file] ||= output
     end
   end
