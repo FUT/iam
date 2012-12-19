38c0144614526684dfe0dd80c4fb59ed400466c4
Yury Kaliada <fut.wrk@gmail.com>
2012-12-19 19:13:05 +0300

    Assets were added to be precompiled
    Signed-off-by: Yury Kaliada <fut.wrk@gmail.com>

--- lib/iam/engine.rb
+++ lib/iam/engine.rb
@@ -1,4 +1,7 @@
 module Iam
   class Engine < ::Rails::Engine
+    initializer :assets do |config|
+      Rails.configuration.assets.precompile += %w(jquery.cookie.js iam.js iam.css)
+    end
   end
 end
