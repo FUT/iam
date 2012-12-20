314fbd7a52c5e74d49fcc86d764eb7d4f0b9b576
Yury Kaliada <fut.wrk@gmail.com>
2012-12-20 18:36:42 +0300

    Configuration was moved to yml
    Signed-off-by: Yury Kaliada <fut.wrk@gmail.com>

--- lib/generators/iam/initializer_generator.rb
+++ lib/generators/iam/initializer_generator.rb
@@ -1,14 +1,12 @@
 module Iam
   module Generators
     class InitializerGenerator < ::Rails::Generators::Base
-
       source_root File.expand_path("../../templates", __FILE__)
 
-      desc 'Creates a sample Iam initializer.'
+      desc 'Creates a sample Iam configuration.'
       def copy_initializer
-        copy_file 'iam.rb', 'config/initializers/iam.rb'
+        copy_file 'iam.yml', 'config/iam.yml'
       end
-
     end
   end
 end
--- lib/iam/configuration.rb
+++ lib/iam/configuration.rb
@@ -1,22 +1,11 @@
 module Iam
   module Configuration
-    def self.configure
-      yield self
+    class << self
+      def config
+        @configuration ||= YAML.load(File.read 'config/iam.yml')
+      end
+
+      delegate :[], to: :config
     end
-
-    mattr_accessor :account_class
-    @@account_class = 'User'
-
-    mattr_accessor :account_attributes
-    @@account_attributes = ['id', 'full_name']
-
-    mattr_accessor :role_class
-    @@role_class = 'Role'
-
-    mattr_accessor :role_name_attribute
-    @@role_name_attribute = 'name'
-
-    mattr_accessor :accounts_for_each_role
-    @@accounts_for_each_role = 3
   end
 end
