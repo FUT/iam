62b0273e51e07de80a088c44b8780a17353d46e3
Pavel Astraukh <paladin111333@yandex.ru>
2013-02-15 20:03:19 +0300

    Possibility to log out
    Signed-off-by: Pavel Astraukh <paladin111333@yandex.ru>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   post 'iam/log_in/:id' => 'iam#log_in', as: :log_in
+  delete 'iam/log_out/:id' => 'iam#log_out', as: :log_out
   get 'iam/menu' => 'iam#menu', as: :iam_menu
 end
--- lib/iam/authorization_provider/devise.rb
+++ lib/iam/authorization_provider/devise.rb
@@ -5,6 +5,10 @@ module Iam
         sign_in Iam::Configuration[:account][:class].downcase, account
       end
 
+      def log_out_account(account)
+        sign_out account
+      end
+
       def current_account?(account)
         current_user == account
       end
