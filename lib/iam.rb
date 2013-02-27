e979d967cd7053e0cd9dc7e49ca79c8c5c0f5b08
Yury Kaliada <fut.wrk@gmail.com>
2013-02-27 22:37:40 +0300

    Iam configuration is checked before loading
    Signed-off-by: Yury Kaliada <fut.wrk@gmail.com>

--- lib/iam.rb
+++ lib/iam.rb
@@ -1,9 +1,13 @@
 require 'rack'
 
-require 'iam/version'
-require 'iam/configuration'
-require 'iam/engine' if defined?(Rails::Engine)
+if File.exists?('config/iam.yml')
+  require 'iam/version'
+  require 'iam/configuration'
+  require 'iam/engine' if defined?(Rails::Engine)
 
-require 'iam/authorization_provider/factory'
+  require 'iam/authorization_provider/factory'
 
-require 'rack/assets_middleware'
+  require 'rack/assets_middleware'
+else
+  raise Exception.new("Iam config file does not exist. You may fix it with: rails g iam:initializer")
+end
