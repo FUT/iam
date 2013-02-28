2003890617b95071a34df46ab578ce00de723a89
yury.kaliada <fut.wrk@gmail.com>
2013-02-28 08:25:17 +0300

    Notification reverted
    Signed-off-by: yury.kaliada <fut.wrk@gmail.com>

--- lib/iam.rb
+++ lib/iam.rb
@@ -1,13 +1,9 @@
 require 'rack'
 
-if File.exists?('config/iam.yml')
-  require 'iam/version'
-  require 'iam/configuration'
-  require 'iam/engine' if defined?(Rails::Engine)
+require 'iam/version'
+require 'iam/configuration'
+require 'iam/engine' if defined?(Rails::Engine)
 
-  require 'iam/authorization_provider/factory'
+require 'iam/authorization_provider/factory'
 
-  require 'rack/assets_middleware'
-else
-  raise Exception.new("Iam config file does not exist. You may fix it with: rails g iam:initializer")
-end
+require 'rack/assets_middleware'
